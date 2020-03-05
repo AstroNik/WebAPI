620ff7a940b24e89a3431610f23410be69cf3cf2
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-04 23:12:11 -0500

    Updated WebAPI Created Go Modules for creating Docker Images

--- db.go
+++ db.go
@@ -1,29 +1,21 @@
 package main
 
 import (
-	"context"
-	//"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"log"
-	"time"
 )
 
-func connectClient() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://devTeam:ecoders4@cluster0-grjmu.azure.mongodb.net/test"))
+func connectClient() *mongo.Client {
+	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://devTeam:ecoders4@cluster0-grjmu.azure.mongodb.net/admin"))
 	if err != nil {
 		log.Fatal(err)
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-
-	//tempDb := client.Database("temp")
-	//userCollection := tempDb.Collection("users")
-	//sensorCollection := tempDb.Collection("sensor")
-
-	//userCollectionResult, err := userCollection.InsertOne(ctx,bson.D{{}})
+	print("Connection Successful\n")
+	return client
 }
+
+//func insertPlantData(sensor Sensor){
+//	client := connectClient()
+//
+//}
--- main.go
+++ main.go
@@ -1,22 +1,36 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", homePage)
 	router.HandleFunc("/getSensorData", dataProcess)
 	router.HandleFunc("/sendSensorData", sendData)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
+func homePage(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome home!")
+}
+
 func dataProcess(w http.ResponseWriter, r *http.Request) {
-	connectClient()
-	//sensorData := Sensor{}
-	//sensorData.DateTime = time.Now()
+	sensorData := Sensor{}
+	sensorData.DateTime = time.Now()
+	reqBody, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot Decode Data")
+	}
+	json.Unmarshal(reqBody, &sensorData)
+
 }
 
 func sendData(w http.ResponseWriter, r *http.Request) {
