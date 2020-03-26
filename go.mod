0771a7f3b68c608251c48e72b5f5ec9109495123
Nikhil <nikhil.kapadia.97@gmail.com>
2020-03-26 15:18:08 -0400

    Updated functions

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/AstroNik/WebAPI
 go 1.13
 
 require (
-	github.com/AstroNik/WebCommon v0.0.0-20200324130018-22b21f77510b
+	github.com/AstroNik/WebCommon v0.0.0-20200326191422-0498e2bbabf0
 	github.com/gorilla/mux v1.7.4
 	go.mongodb.org/mongo-driver v1.3.1
 )
--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -15,11 +16,13 @@ func main() {
 	router.HandleFunc("/", homePage)
 	router.HandleFunc("/getSensorData", sendData)
 	router.HandleFunc("/dataProcess", dataProcess)
+	router.HandleFunc("/signup", signup)
+	router.HandleFunc("/signin", signin)
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
 func homePage(w http.ResponseWriter, r *http.Request) {
-	db.ConnectClient()
+
 }
 
 func sendData(w http.ResponseWriter, r *http.Request) {
@@ -47,12 +50,27 @@ func dataProcess(w http.ResponseWriter, r *http.Request) {
 	log.Print(sensorData)
 
 	//The BELOW is how the data will be Inserted into the Database
-	//currTime := time.Now().Format(time.RFC3339) //Time is in UTC Format
-	//sensor := structs.Sensor{
-	//	SensorId:   0, //will correspond with the device sending the data
-	//	SensorName: "", //will have to have somewhere we can set this data up in the app
-	//	DateTime:   currTime,
-	//	SensorData: sensorData,
-	//}
-	//db.InsertMoistureData(sensor)
+	currTime := time.Now() //.Format(time.RFC3339) //Time is in UTC Format
+	sensor := structs.Sensor{
+		SensorId:   0,                 //will correspond with the device sending the data
+		SensorName: "Moisture Sensor", //will have to have somewhere we can set this data up in the app
+		DateTime:   currTime,
+		SensorData: sensorData,
+	}
+
+	//temporary customerID
+	db.InsertMoistureData("234556314", sensor)
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+	//TODO: Decode data being sent for application
+	//format data into correct format
+	//encrypt password
+	//store in db
+
+}
+func signin(w http.ResponseWriter, r *http.Request) {
+	//check if user exist
+	//allow access
+
 }
