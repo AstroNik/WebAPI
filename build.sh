a98c132a10572b5ecede7a9e8dc53b6e6b72a01b
Nikhil <nikhil.kapadia.97@gmail.com>
2020-05-15 14:09:49 -0400

    updated build.sh for local running

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #! /bin/sh
 
-docker build . -t astronik/webservice:0.1
+docker build . -t astronik/webservice:latest
+
+
+kubectl delete deploy ecoders-webapi
+kubectl delete svc ecoders-webapi-service
+
+kubectl apply -f ./k8s/webservicelocal-deployment.yaml
+kubectl apply -f ./k8s/webservicelocal-service.yaml
