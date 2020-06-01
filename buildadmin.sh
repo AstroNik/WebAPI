48d0a7076a4ecaf5d2b3fc255d605aa5d3827250
Nikhil <nikhil.kapadia.97@gmail.com>
2020-06-01 16:52:23 -0400

    Added build for admin

---
+++ buildadmin.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+cd admin
+
+npm run build
--- startService.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/sh
-
-./build.sh
-echo ""
-kubectl apply -f ./k8s/webservicelocal-deployment.yaml
-kubectl apply -f ./k8s/webservice-service.yaml
