44fac4472299507402d91f6f21c491c8aecba41d
Nikhil <nikhil.kapadia.97@gmail.com>
2020-08-27 12:21:47 -0400

    Added Redirect (Response Errors)

--- backend/auth.go
+++ backend/auth.go
@@ -19,12 +19,10 @@ func HandleSecureFunc(handler http.HandlerFunc) http.HandlerFunc {
 		_ = json.NewEncoder(w).Encode(r)
 		header = strings.TrimSpace(header)
 
-		//Different check will be done here
 		if header == "" {
 			log.Print("EMPTY HEADER")
-			//w.WriteHeader(http.StatusForbidden)
-			////_ = json.NewEncoder(w).Encode("Missing Token")
-			//RETURN SOME STATUS TO SHOW IN FRONT END OR SOMETHING
+			w.WriteHeader(http.StatusForbidden)
+			_ = json.NewEncoder(w).Encode("Empty Header")
 			return
 		}
 
@@ -46,8 +44,9 @@ func HandleSecureFunc(handler http.HandlerFunc) http.HandlerFunc {
 
 		token, err := client.VerifyIDToken(ctx, header)
 		if err != nil {
-			log.Fatalf("error verifying ID token: %v\n", err)
-			//return response here if failed to validate token.
+			log.Printf("error verifying ID token: %v\n", err)
+			w.WriteHeader(http.StatusUnauthorized)
+			_ = json.NewEncoder(w).Encode("401")
 		}
 
 		log.Printf("Verified ID token: %v\n", token)
